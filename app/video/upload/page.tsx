721e0d7318a7e23e4079b8426bd2b170806d6383
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-11 02:09:05 +0530

    added setup of imagekit

---
+++ app/video/upload/page.tsx
@@ -0,0 +1,13 @@
+import UploadFile from '@/components/utils/UploadFile'
+
+const UploadVideo = () => {
+  return (
+    <div>
+        <div>
+            <UploadFile />
+        </div>
+    </div>
+  )
+}
+
+export default UploadVideo
