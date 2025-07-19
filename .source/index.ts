2ba55422f803c0bf3aade7bb00bb8b3558615c26
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-19 16:10:01 +0530

    removed alert on lesson 2

--- .source/index.ts
+++ .source/index.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as docs_1 from "../content/docs/wear-seatbelts-and-helmets.mdx?collection=docs&hash=1751981228980"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1751981228980"
+import * as docs_2 from "../content/docs/wear-seatbelts-and-helmets.mdx?collection=docs&hash=1751981228980"
+import * as docs_1 from "../content/docs/index.mdx?collection=docs&hash=1751981228980"
+import * as docs_0 from "../content/docs/follow-traffic-signals-and-road-signs.mdx?collection=docs&hash=1751981228980"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"D:/Bhaiyaa/Dev-2025/projects/roadha/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"wear-seatbelts-and-helmets.mdx","absolutePath":"D:/Bhaiyaa/Dev-2025/projects/roadha/content/docs/wear-seatbelts-and-helmets.mdx"}, data: docs_1 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"follow-traffic-signals-and-road-signs.mdx","absolutePath":"D:/Bhaiyaa/Dev-2025/projects/roadha/content/docs/follow-traffic-signals-and-road-signs.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"D:/Bhaiyaa/Dev-2025/projects/roadha/content/docs/index.mdx"}, data: docs_1 }, { info: {"path":"wear-seatbelts-and-helmets.mdx","absolutePath":"D:/Bhaiyaa/Dev-2025/projects/roadha/content/docs/wear-seatbelts-and-helmets.mdx"}, data: docs_2 }], [])
