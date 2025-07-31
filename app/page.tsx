d4c8106f700fa5a732ad06f7c4d5b92ae3592a24
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-31 21:19:42 +0530

    feat: added faq on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { BentoGrid1 } from "@/components/layouts/BentoGrid1";
 import Footer from "@/components/layouts/Footer";
 import Navbar from "@/components/layouts/Navbar";
 import TriviaPopup from "@/components/popups/TriviaPopup";
+import FAQ from "@/components/sections/home/FAQ";
 import Hero from "@/components/sections/home/Hero";
 
 export default function Home() {
@@ -11,6 +12,17 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] items-center sm:items-start">
         <Hero />
         <BentoGrid1 />
+        <section className="relative py-30 overflow-hidden w-full">
+          <div className="absolute -bottom-28 -left-28 w-96 h-96 bg-cyan-400/20 rounded-full blur-2xl opacity-25 pointer-events-none z-0"></div>
+          <div className="absolute top-10 right-0 w-72 h-72 bg-zinc-400/20 rounded-full blur-2xl opacity-30 pointer-events-none z-0"></div>
+
+          <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight text-center mb-8">
+            FAQs
+          </h2>
+          <section className="max-w-3xl mx-auto px-8">
+            <FAQ />
+          </section>
+        </section>
         <TriviaPopup />
       </main>
       <Footer />
