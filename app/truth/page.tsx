4c5b2a91c37d4df18db34f3ccd84a1c927ecfe97
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-01 23:10:51 +0530

    added truth page

--- app/truth/page.tsx
+++ app/truth/page.tsx
@@ -1,33 +1,82 @@
 import H1 from "@/components/text/H1";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "The Truth | Roadha",
+  description: "What's wrong with us?",
+};
 
 const TruthPage = () => {
   return (
-    <section className="flex flex-col gap-[32px] px-8 py-20 w-full max-w-[1440px] mx-auto">
-      <H1 title="What's wrong with India?" />
+    <section className="flex flex-col gap-[32px] px-8 py-20 md:py-32 max-w-[1440px] mx-auto">
+      <H1 title="We are just clueless on roads." />
 
-      <section className="max-w-2xl space-y-4 mt-8">
-        <p>It is simple.</p>
+      <section className="max-w-xl space-y-4 mt-8">
         <p>
-          Indians keep on buying more & more cars. But have their brains gotten
-          bigger with the road safety knowledge too?
+          We keep buying more and more vehicles. But have our brains gotten
+          bigger with road safety knowledge too?
         </p>
-        <p>Not so much.</p>
+
+        <p>Not at all.</p>
+
+        <p>You know it. I know it.</p>
+
         <p>
-          According to MoRTH,{" "}
-          <span className="text-4xl font-bold text-red-400">153,972</span>{" "}
-          persons were{" "}
-          <a
-            href="https://tripc.iitd.ac.in/assets/publication/RSI_2023_web.pdf"
-            className="underline hover:no-underline"
-            target="_blank"
-          >
-            killed in road crashes
-          </a>{" "}
-          in the year 2021.
+          According to the Ministry of Road Transport and Highways (MoRTH),{" "}
+          <strong>
+            over 1.5 lakh people were killed in road crashes in 2021 alone
+          </strong>
+          . That’s <strong>one person every 3.5 minutes</strong>.
         </p>
-        <p>Are these increasing year by year?</p>
-        <p>Yes.</p>
-        <p>Hence, Roadha.</p>
+
+        <p>What’s worse?</p>
+        <p>
+          These numbers <strong>keep increasing</strong> every year.
+        </p>
+        <p>And most of these accidents? </p>
+
+        <p>
+          They’re avoidable: caused by overspeeding, ignorance, and lack of
+          basic awareness.
+        </p>
+
+        <p>
+          <strong>So, why Roadha?</strong>
+        </p>
+
+        <p>
+          Because there’s no easy, fun, and India-specific way to{" "}
+          <strong>learn road safety</strong>.
+        </p>
+
+        <p>We don’t teach it in schools.</p>
+        <p>We don’t re-learn it before buying a new car.</p>
+        <p>We don’t quiz ourselves before hitting the highway.</p>
+
+        <p>
+          <strong>
+            <span className="bg-green-400 text-black px-1.5 mx-0.5 rounded-xs select-none">
+              Roadha
+            </span>
+          </strong>{" "}
+          is India’s first road safety learning platform
+        </p>
+        {/* <ul className="list-disc list-inside pl-4">
+          <li>Breaks down rules into simple, relatable tips 📘</li>
+          <li>
+            Makes safety learning interactive with quizzes and scenarios ✅
+          </li>
+          <li>
+            Will eventually let users <strong>track their vehicles</strong>,{" "}
+            <strong>store documents</strong>, and{" "}
+            <strong>compete on leaderboards</strong> — all for building a
+            smarter driving culture. 🏆
+          </li>
+        </ul> */}
+
+        <p className="text-2xl">
+          <strong>Not just a platform. A public movement.</strong>
+        </p>
       </section>
     </section>
   );
