313a798e61437aedbaa7dc338aee3a1d27c67f4c
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-03 18:51:57 +0530

    removed console log

--- supabase/fetchDb.ts
+++ supabase/fetchDb.ts
@@ -2,7 +2,6 @@ import supabase from "./supabase";
 
 export const getSources = async () => {
   const { data, error } = await supabase.from("sources").select();
-  console.log({ data });
 
   return { data, error };
 };
