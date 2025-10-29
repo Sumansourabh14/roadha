d9c8a5ca063ac499382fda40ef86ce94f36f7df2
Suman Sourabh <sumsourabh14@gmail.com>
2025-10-29 19:34:21 +0530

    added a folder group

--- .source/index.ts
+++ .source/index.ts
@@ -1,7 +1,8 @@
 // @ts-nocheck -- skip type checking
+import * as docs_3 from "../content/docs/(basic-must-knows)/do-not-use-mobile-phones-while-driving.mdx?collection=docs&hash=1761662319555"
 import * as docs_2 from "../content/docs/wear-seatbelts-and-helmets.mdx?collection=docs&hash=1761662319555"
 import * as docs_1 from "../content/docs/index.mdx?collection=docs&hash=1761662319555"
 import * as docs_0 from "../content/docs/follow-traffic-signals-and-road-signs.mdx?collection=docs&hash=1761662319555"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"follow-traffic-signals-and-road-signs.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/follow-traffic-signals-and-road-signs.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/index.mdx"}, data: docs_1 }, { info: {"path":"wear-seatbelts-and-helmets.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/wear-seatbelts-and-helmets.mdx"}, data: docs_2 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"follow-traffic-signals-and-road-signs.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/follow-traffic-signals-and-road-signs.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/index.mdx"}, data: docs_1 }, { info: {"path":"wear-seatbelts-and-helmets.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/wear-seatbelts-and-helmets.mdx"}, data: docs_2 }, { info: {"path":"(basic-must-knows)\\do-not-use-mobile-phones-while-driving.mdx","absolutePath":"C:/Testing-1/code/roadha/content/docs/(basic-must-knows)/do-not-use-mobile-phones-while-driving.mdx"}, data: docs_3 }], [])
