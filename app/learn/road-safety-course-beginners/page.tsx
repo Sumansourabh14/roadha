01db88a22d9b2bef79f24eb66b0348508564de9f
Suman Sourabh <sumsourabh14@gmail.com>
2025-10-30 23:18:26 +0530

    added description for course page

--- app/learn/road-safety-course-beginners/page.tsx
+++ app/learn/road-safety-course-beginners/page.tsx
@@ -1,15 +1,20 @@
 import ItemTable from "@/components/tables/ItemTable";
 import H1 from "@/components/text/H1";
-import roadSafetyCourseBegineerData from "@/data/road_safety_rules_with_tags.json";
+import roadSafetyCourseBeginnerData from "@/data/road_safety_rules_with_tags.json";
 
 const RoadSafety = () => {
   return (
     <section className="px-8">
-      <section className="pt-24 pb-8">
-        <H1 title="Road Safety Course - Beginners" />
+      <section className="pt-24 pb-8 text-center space-y-8">
+        <H1 title="Road Safety Course - Beginner" />
+        <p className="text-lg">
+          FREE Step-by-step course covering road safety rules, tips & scenarios
+          in India.
+        </p>
       </section>
-      <section className="pt-20 pb-32">
-        <ItemTable data={roadSafetyCourseBegineerData} />
+      <hr />
+      <section className="pt-12 pb-32">
+        <ItemTable data={roadSafetyCourseBeginnerData} />
       </section>
     </section>
   );
