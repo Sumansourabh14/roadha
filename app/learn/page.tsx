83ffefca0a169895e9be52f3bdcafa5e40f3f624
Suman Sourabh <sumsourabh14@gmail.com>
2025-11-02 21:43:26 +0530

    updated lesson count on learn

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -35,7 +35,7 @@ const Learn = () => {
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
-              <p className="text-sm">8/51 lessons</p>
+              <p className="text-sm">9/51 lessons</p>
               <Badge variant="default">Beginner</Badge>
             </CardContent>
           </Card>
