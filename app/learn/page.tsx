258e8e82e24ad4e06c7412531ca25dc122864128
Suman Sourabh <sumsourabh14@gmail.com>
2025-11-05 21:37:41 +0530

    updated lesson numbers

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -35,7 +35,7 @@ const Learn = () => {
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
-              <p className="text-sm">11/51 lessons</p>
+              <p className="text-sm">12/51 lessons</p>
               <Badge variant="default">Beginner</Badge>
             </CardContent>
           </Card>
