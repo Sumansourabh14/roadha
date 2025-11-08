6eeaa522280f7650aa6390fd62da72659367799a
Suman Sourabh <sumsourabh14@gmail.com>
2025-11-08 20:10:53 +0530

    added truth translation on home

--- components/sections/home/Truth.tsx
+++ components/sections/home/Truth.tsx
@@ -1,16 +1,19 @@
 import H2 from "@/components/text/H2";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 const Truth = () => {
+  const t = useTranslations("Truth");
+
   return (
     <section className="max-w-[1000px] py-20 px-8 mx-auto">
       <div className="max-w-5xl mx-auto mb-12 text-center">
-        <H2 title="Are the accidents really that much?" />
+        <H2 title={t("title")} />
         <Link
           href={`/truth`}
           className="py-4 px-12 bg-green-300 text-black rounded-sm font-bold text-xl"
         >
-          Sadly, yes
+          {t("button")}
         </Link>
       </div>
     </section>
